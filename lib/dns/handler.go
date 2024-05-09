121f031a8190edea82591a38c0ccc8b0eb811366
임수현 <ish@innogrid.com>
2024-05-09 15:33:29 +0900

    lib: dns: Disable recusrion

--- lib/dns/handler.go
+++ lib/dns/handler.go
@@ -6,6 +6,10 @@ func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
 	m := new(dns.Msg)
 	m.SetReply(r)
 	m.Compress = false
+	m.RecursionDesired = true
+
+	// Disable recursion to find next server from the client.
+	m.RecursionAvailable = false
 
 	switch r.Opcode {
 	case dns.OpcodeQuery:
